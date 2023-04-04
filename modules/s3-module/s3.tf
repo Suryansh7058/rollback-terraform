2f8e143e9d675182db1b68c931c1f562783bfe5a
SuryanshSingh <suryansh.singh1@pens.com>
2023-04-04 15:04:00 +0530

    Adding a CDN module

--- modules/s3-module/s3.tf
+++ modules/s3-module/s3.tf
@@ -1,3 +1,23 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = local.bucket_name
 }
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "policy" {
+  policy = jsonencode({
+
+  })
+}
